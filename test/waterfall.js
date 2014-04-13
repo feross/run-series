b62bd433766147ca6aa524f8fa0205ffbc020e3a
Feross Aboukhadijeh <feross@feross.org>
2014-04-12 18:58:34 -0700

    add failing test for waterfall

---
+++ test/waterfall.js
@@ -0,0 +1,29 @@
+var series = require('../')
+var test = require('tape')
+
+test('waterfall: functions pass results to next function', function (t) {
+  t.plan(8)
+
+  var tasks = [
+    function (cb) {
+      t.pass('cb 1')
+      cb(null, 1)
+    },
+    function (result, cb) {
+      t.equal(result, 1)
+      cb(null, 2, 3, 4)
+    },
+    function (result1, result2, result3, cb) {
+      t.equal(result1, 2)
+      t.equal(result2, 3)
+      t.equal(result3, 4)
+      cb(null, 99, 100)
+    }
+  ]
+
+  series.waterfall(tasks, function (err, result1, result2) {
+    t.error(err)
+    t.equal(result1, 99)
+    t.equal(result2, 100)
+  })
+})
