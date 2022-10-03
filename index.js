22422cb5d79aef9885aa4d2713b95d1cd3cf3473
Mohamed Shelf <60757136+MohamedSaid7102@users.noreply.github.com>
2022-10-03 09:24:41 +0200

    Made 'runSeries' function more robust.
    - [x] Error handling by 'type checking', If passed tasks are an array or not.
    
    - [x] Check if the following array to call is callable or not and if not call the next one in a recursion order.

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 /*! run-series. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 module.exports = runSeries
 
-function runSeries (tasks, cb) {
-  let current = 0
-  const results = []
-  let isSync = true
+function runSeries(tasks, cb) {
+  // First check if passed tasks are of type 'Array'
+  if (!Array.isArray(tasks))
+    throw TypeError('You need to pass an array of functions..!');
 
-  function done (err) {
-    function end () {
-      if (cb) cb(err, results)
+  let current = 0;
+  const results = [];
+  let isSync = true;
+
+  function done(err) {
+    function end() {
+      // Always check if provided callback function is callable (is an actual function)
+      if (cb instanceof Function) cb(err, results);
     }
-    if (isSync) process.nextTick(end)
-    else end()
+
+    if (isSync) process.nextTick(end);
+    else end();
   }
 
-  function each (err, result) {
-    results.push(result)
-    if (++current >= tasks.length || err) done(err)
-    else tasks[current](each)
+  function excuteTask(task) {
+    if (current > tasks.length) return; /** Base case */
+
+    if (task instanceof Function) task(each);
+    else
+      excuteTask(
+        tasks[++current]
+      ); /** Recursion till find a callable, or exit if not and reached the end of tasks array */
   }
 
-  if (tasks.length > 0) tasks[0](each)
-  else done(null)
+  function each(err, result) {
+    /** Check first if the function returned a result, so that returned array 'result' doesn't has something like [ 1, undefined, 2, 4 ] */
+    if (result !== undefined) results.push(result);
 
-  isSync = false
+    if (current >= tasks.length || err) done(err);
+    else excuteTask(tasks[++current]);
+  }
+
+  // Always check if current item is a is callable (is an actual function).
+  if (tasks.length > 0) excuteTask(tasks[current]);
+  else done(null);
+
+  isSync = false;
 }
